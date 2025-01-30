77e791c75e2c4fe58fa603d018b1fdce598f4cf0
wendy munyasi <wendymunyasi@gmail.com>
2025-01-30 16:29:27 +0300

    Add viewsets for the serializers

--- hotelBookingApp/views.py
+++ hotelBookingApp/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+"""Module for views for each serializer
+"""
 
-# Create your views here.
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Room, Booking
+from .serializers import BookingSerializer, RoomSerializer
+
+class RoomViewSet(viewsets.ModelViewSet):
+    """A simple viewSet for viewing rooms
+    """
+    queryset = Room.objects.all() # pylint: disable=no-member
+    serializer_class = RoomSerializer
+
+class BoookingViewSet(viewsets.ModelViewSet):
+    """A simple viewSet for viewing Bookings
+    """
+    queryset = Booking.objects.all() # pylint: disable=no-member
+    serializer_class = BookingSerializer
