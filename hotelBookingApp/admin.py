4144c261e8046f28259251d70916d3571be16864
wendy munyasi <wendymunyasi@gmail.com>
2025-02-01 23:30:28 +0300

    Register payment model in admin

--- hotelBookingApp/admin.py
+++ hotelBookingApp/admin.py
@@ -3,7 +3,8 @@
 
 from django.contrib import admin
 
-from .models import Booking, Room
+from .models import Booking, Room, Payment
 
 admin.site.register(Booking)
 admin.site.register(Room)
+admin.site.register(Payment)
